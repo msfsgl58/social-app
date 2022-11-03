69c0ca7cc7ccaea49dc3359340841c8f2364bac5
msfsgl58 <msf_1848@hotmail.com>
2022-11-04 01:02:03 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background'>
+    <div className='container'>
+      <h3 className='social-app'>Social APP</h3>
+    </div>
     </div>
   );
 }
