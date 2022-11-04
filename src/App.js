336ec1da39f0dda3d927694e27efd1831056a89c
msfsgl58 <msf_1848@hotmail.com>
2022-11-04 05:34:05 +0300

    İnstall React router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import LeftMenu from './menuFolder/left-menu'
-import RightMenu from './menuFolder/right-menu'
+import {Routes, Route} from "react-router-dom"
+import HomePage from "./HomePage";
+import ProfilePage from './ProfilePage'
 
 function App() {
   return (
-    <div className="background">
-      <LeftMenu />
-      <div className="container"></div>
-      <RightMenu />
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profil" element={<ProfilePage />} />
+      </Routes>
   );
 }
 
---
+++ src/ProfilePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css'
+import LeftMenu from "./menuFolder/left-menu";
+
+function ProfilePage() {
+    return(
+        <div className="background">
+        <LeftMenu />
+        <div className="container"></div>
+        </div>
+    )
+}
+export default ProfilePage;
